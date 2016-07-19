83200f367ddb1966cf4064d17a3531c625aa4d69
Sue Clark <susanx.r.clark@intel.com>
2016-07-19 08:55:36 -0700

    Information about re-use of panel panes.

---
+++ entities_with_same_block.sql
@@ -0,0 +1,22 @@
+drop temporary table if exists published_content, published_panelizer_nodes;
+
+/* Get the current published version of the workbench moderated content */
+create temporary table if not exists published_content (
+select nh.nid, nh.vid from workbench_moderation_node_history as nh
+where nh.published = 1);
+
+/* Get the current published version of nodes that use panelizer */
+create temporary table if not exists published_panelizer_nodes (
+select pe.entity_id, pe.did from panelizer_entity as pe
+left join published_content as pc on (pc.nid=pe.entity_id and pc.vid=pe.revision_id)
+where pc.nid is not null);
+
+/* Get the panes in the panelizer content */
+create temporary table if not exists panel_info (
+select ppn.*, pp.pid, pp.panel, pp.type, pp.subtype, pp.shown from panels_pane as pp 
+left join published_panelizer_nodes as ppn on ppn.did=pp.did
+where ppn.entity_id is not null);
+
+select pi.entity_id, pi.subtype from panel_info as pi
+where type='block'
+order by pi.subtype
